12929ea0a0c7d0f61ade67c37970fbdf376fcd02
OmAr_AbouZeid <92922335+oabouzeid@users.noreply.github.com>
2026-04-22 23:24:54 +0200

    Remove unused comment in validateResults.js
    Remove

--- task_management_system/validations/validateResults.js
+++ task_management_system/validations/validateResults.js
@@ -4,7 +4,6 @@ import HttpError from "../utils/httpError.js";
 export default (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        // fix from demo: error was built but never properly forwarded
         const error = new HttpError(400, "validation error");
         error.errors = errors.array();
         return next(error);
